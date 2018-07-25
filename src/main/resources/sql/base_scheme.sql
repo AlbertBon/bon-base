0af7fee19856cad12c42c74346b37c7e6cc7d03b
AlertBon <502285815@qq.com>
2018-07-25 16:32:49 +0800

    基础数据库语句修改

--- src/main/resources/sql/base_scheme.sql
+++ src/main/resources/sql/base_scheme.sql
@@ -2,23 +2,20 @@
 -- 主机:                           127.0.0.1
 -- 服务器版本:                        5.5.28 - MySQL Community Server (GPL)
 -- 服务器操作系统:                      Win32
--- HeidiSQL 版本:                  9.5.0.5196
+-- HeidiSQL 版本:                  9.3.0.4984
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-
 -- 导出 wxmanage 的数据库结构
-DROP DATABASE IF EXISTS `wxmanage`;
 CREATE DATABASE IF NOT EXISTS `wxmanage` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `wxmanage`;
 
+
 -- 导出  表 wxmanage.menu 结构
-DROP TABLE IF EXISTS `menu`;
 CREATE TABLE IF NOT EXISTS `menu` (
   `menu_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
@@ -34,11 +31,12 @@ CREATE TABLE IF NOT EXISTS `menu` (
   `data_path` varchar(512) DEFAULT NULL COMMENT '数据库id地址',
   `parent` bigint(20) DEFAULT NULL COMMENT '父菜单id',
   PRIMARY KEY (`menu_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='菜单表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='菜单表';
 
 -- 数据导出被取消选择。
+
+
 -- 导出  表 wxmanage.permission 结构
-DROP TABLE IF EXISTS `permission`;
 CREATE TABLE IF NOT EXISTS `permission` (
   `permission_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
@@ -47,13 +45,15 @@ CREATE TABLE IF NOT EXISTS `permission` (
   `permission_name` varchar(32) DEFAULT NULL COMMENT '权限名称',
   `type` char(2) DEFAULT NULL COMMENT '00:菜单权限',
   `object_id` bigint(20) DEFAULT NULL COMMENT '对应表id（菜单权限即为菜单id）',
+  `object_parent` bigint(20) DEFAULT NULL COMMENT '对应表id的父id（菜单权限即为菜单id的父id）',
   PRIMARY KEY (`permission_id`),
   UNIQUE KEY `permission_flag` (`permission_flag`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='权限表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='权限表';
 
 -- 数据导出被取消选择。
+
+
 -- 导出  表 wxmanage.role 结构
-DROP TABLE IF EXISTS `role`;
 CREATE TABLE IF NOT EXISTS `role` (
   `role_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
@@ -62,11 +62,12 @@ CREATE TABLE IF NOT EXISTS `role` (
   `role_flag` varchar(32) DEFAULT NULL COMMENT '角色标识',
   PRIMARY KEY (`role_id`),
   UNIQUE KEY `role_flag` (`role_flag`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='角色表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
 
 -- 数据导出被取消选择。
+
+
 -- 导出  表 wxmanage.role_permission 结构
-DROP TABLE IF EXISTS `role_permission`;
 CREATE TABLE IF NOT EXISTS `role_permission` (
   `role_permission_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
@@ -74,11 +75,12 @@ CREATE TABLE IF NOT EXISTS `role_permission` (
   `permission_id` bigint(20) DEFAULT NULL COMMENT '权限id',
   `role_id` bigint(20) DEFAULT NULL COMMENT '角色id',
   PRIMARY KEY (`role_permission_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COMMENT='角色权限表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色权限表';
 
 -- 数据导出被取消选择。
+
+
 -- 导出  表 wxmanage.sys_base 结构
-DROP TABLE IF EXISTS `sys_base`;
 CREATE TABLE IF NOT EXISTS `sys_base` (
   `sys_base_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
@@ -98,8 +100,9 @@ CREATE TABLE IF NOT EXISTS `sys_base` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='数据库基础表，包含所有数据库信息';
 
 -- 数据导出被取消选择。
+
+
 -- 导出  表 wxmanage.user 结构
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `user_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
@@ -117,11 +120,12 @@ CREATE TABLE IF NOT EXISTS `user` (
   `secret_key` char(64) DEFAULT NULL COMMENT '密钥',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='用户表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 -- 数据导出被取消选择。
+
+
 -- 导出  表 wxmanage.user_role 结构
-DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE IF NOT EXISTS `user_role` (
   `user_role_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `gmt_create` datetime DEFAULT NULL COMMENT '创建时间',
@@ -129,7 +133,7 @@ CREATE TABLE IF NOT EXISTS `user_role` (
   `user_id` bigint(20) DEFAULT NULL COMMENT '用户id',
   `role_id` bigint(20) DEFAULT NULL COMMENT '角色id',
   PRIMARY KEY (`user_role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='用户角色映射表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户角色映射表';
 
 -- 数据导出被取消选择。
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
